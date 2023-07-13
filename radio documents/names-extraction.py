2486489a29105defb00b6b1cb04fb72200dba65f
JA Pryse <54377063+prys0000@users.noreply.github.com>
2023-07-13 11:12:26 -0500

    Add files via upload

--- radio documents/names-extraction.py
+++ radio documents/names-extraction.py
@@ -5,16 +5,19 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 # Load the Excel data into a pandas DataFrame
-df = pd.read_excel('data.xlsx')  # Replace with your Excel file path
+df = pd.read_excel(r'D:\GITHUB\Practice\audio-recog-output1.xlsx')  # Replace with your Excel file path
 
-# Function to extract proper nouns from a text
-def extract_proper_nouns(text):
-    doc = nlp(text)
-    return [ent.text for ent in doc.ents if ent.label_ in ['PERSON', 'ORG', 'GPE', 'LOC', 'NORP']]
+# Check if 'Text' column exists in the DataFrame
+if 'Text' in df.columns:
+    # Function to extract proper nouns from a text
+    def extract_proper_nouns(text):
+        doc = nlp(text)
+        return [ent.text for ent in doc.ents if ent.label_ in ['PERSON', 'ORG', 'GPE', 'LOC', 'NORP']]
 
-# Apply the function to each cell in the DataFrame
-for column in df.columns:
-    df[f'{column}_proper_nouns'] = df[column].apply(extract_proper_nouns)
+    # Apply the function to the 'Text' column
+    df['Text_proper_nouns'] = df['Text'].apply(extract_proper_nouns)
 
-# Write the DataFrame back to Excel
-df.to_excel('data_with_proper_nouns.xlsx', index=False)
+    # Write the DataFrame back to Excel
+    df.to_excel('data_with_proper_nouns.xlsx', index=False)
+else:
+    print("Column 'Text' does not exist in the DataFrame.")
